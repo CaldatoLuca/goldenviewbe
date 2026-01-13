103c7f2f3db5037385c9bd7967cfe66993a4a64d
CaldatoLuca <lucacaldato15@gmail.com>
2026-01-14 00:36:09 +0100

    prisma connection

--- dist/server.js
+++ dist/server.js
@@ -1,9 +1,22 @@
 import express, {} from "express";
 import { notFound } from "./middleware/notFound.js";
 import { errorHandler } from "./middleware/errorHandler.js";
+import prisma from "./config/prisma.js";
 const app = express();
 app.get("/", (req, res) => {
     res.send("Server is running");
+});
+app.get("/test-db", async (req, res) => {
+    try {
+        const spot = await prisma.spot.findFirst();
+        if (!spot)
+            return res.json({ message: "Nessun record trovato" });
+        res.json(spot);
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).json({ error: "Errore connessione DB" });
+    }
 });
 app.use(notFound);
 app.use(errorHandler);
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,24 @@
 import express, { type Request, type Response } from "express";
 import { notFound } from "./middleware/notFound.js";
 import { errorHandler } from "./middleware/errorHandler.js";
+import prisma from "./config/prisma.js";
 
 const app = express();
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Server is running");
+});
+
+//controlla che avda la connessione
+app.get("/test-db", async (req: Request, res: Response) => {
+  try {
+    const spot = await prisma.spot.findFirst();
+    if (!spot) return res.json({ message: "Nessun record trovato" });
+    res.json(spot);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "Errore connessione DB" });
+  }
 });
 
 app.use(notFound);
