6a4b4c3c226d30a104a69bf9d2c9c65d0491a3f0
CaldatoLuca <lucacaldato15@gmail.com>
2026-03-08 18:38:42 +0100

    reinstall

---
+++ prisma/migrations/20260308172829_add_index_lat_lng/migration.sql
@@ -0,0 +1,2 @@
+-- CreateIndex
+CREATE INDEX "Spot_latitude_longitude_idx" ON "Spot"("latitude", "longitude");
--- src/server.ts
+++ src/server.ts
@@ -18,6 +18,7 @@ const app = express();
 
 const allowedOrigins = [
   "http://localhost:3000",
+  "http://localhost:3001",
   "https://goldenview-admin.netlify.app",
   "https://goldenviewadmin.netlify.app",
   "https://gw-admin-livid.vercel.app",
